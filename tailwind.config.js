8385c9efb016c10e8134411f8aee921d4bfff0ab
Lassi Köykkä <lassi_koykka@hotmail.com>
2024-11-23 13:00:59 +0200

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./ui/input.css",
+    "./views/**/*.{html,js}",
+    "./static/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
